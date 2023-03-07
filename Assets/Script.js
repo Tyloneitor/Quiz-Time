57eef40620927daf5cc2c8ea65f3e024f55a41fe
tyloneitor <rodri329@go.stockton.edu>
2023-03-06 19:14:40 -0500

    Structure and questions

---
+++ Assets/Script.js
@@ -0,0 +1,30 @@
+//function timer() {
+   // var start = new Date();
+    //document.querySelector('input[name="timer"|').value = start;
+    //}
+//^^6 timer function
+
+Var Question = [{question:"Coprastastaphobia is the fear of what?",
+Option:["Holes","Constipation","Clowns","Dirt/Dust"],
+answer: "Constipation"
+},
+Var Question = [{question:" In Texas, it’s illegal to swear in front of what?",
+Option:["A Drag Queen","A child","A Corpse","A pastor"],
+answer: "A Corpse"
+},
+Var Question = [{question:"What was Marilyn Monroe’s natural hair color?",
+Option:["Blonde","Red","Brown","Black"],
+answer: "Red"
+},
+Var Question = [{question:"Coprastastaphobia is the fear of what?",
+Option:["Holes","Constipation","Clowns","Dirt/Dust"],
+answer: "Constipation"
+},
+Var Question = [{question:"Coprastastaphobia is the fear of what?",
+Option:["Holes","Constipation","Clowns","Dirt/Dust"],
+answer: "Constipation"
+},
+
+
+];
+https://chat.openai.com/chat/bd9e49f7-0075-4590-aa90-d414396bea93
